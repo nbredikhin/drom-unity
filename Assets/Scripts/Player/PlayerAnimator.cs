871b56908f2cf86ef43ab6d118c773ca47cd1b29
Ivanov Dmitry Viktorovich <dddvvv1997@gmail.com>
2019-08-24 23:39:06 +0000

    Example code for 2.5d game

---
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    [Header("References")] 
+    
+    public Animator anim; 
+        
+    public PlayerController pc;
+
+    private void Awake()
+    {
+        if (pc == null)
+        {
+            gameObject.GetComponent<PlayerController>();
+        }
+    }
+
+    void Update()
+    {
+        MoveAnim();
+    }
+    
+    void MoveAnim()
+    {
+        if (pc.MovementDirection != Vector2.zero)
+        {
+            anim.SetFloat("Horizontal", pc.MovementDirection.normalized.x);
+            anim.SetFloat("Vertical", pc.MovementDirection.normalized.y);
+        }
+
+        anim.SetFloat("Speed",pc.MovementSpeed);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Movement Stats")]
+    
+    [Range(1,500)]public float MOVEMENT_BASE_SPEED;
+    
+    [Space(10)]
+    
+    [Header("Movement State")] 
+    
+    [SerializeField]private float _movementSpeed;
+
+    public float MovementSpeed => _movementSpeed;
+
+    [SerializeField]private Vector2 _movementDirection;
+
+    public Vector2 MovementDirection => _movementDirection;
+
+    [SerializeField]private Vector2 _mouseDirection;
+
+    public Vector2 MouseDirection => _mouseDirection;
+
+    [Space(10)]
+    
+    [Header("References")] 
+    
+    public Rigidbody2D rb;
+
+    private void Awake()
+    {
+        if (rb == null)
+        {
+            gameObject.GetComponent<Rigidbody2D>();
+        }
+    }
+    
+    void Update()
+    {
+        ProcessInput();
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        //movement direction processing
+        _movementDirection = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"));
+        _movementSpeed = Mathf.Clamp(_movementDirection.magnitude,0.0f,1.0f);
+        _movementDirection.Normalize();
+        
+        //mouse direction processing
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _mouseDirection = new Vector2(mousePosition.x - transform.position.x,mousePosition.y-transform.position.y);
+        _mouseDirection.Normalize();
+
+    }
+
+    void Move()
+    {
+        rb.velocity = _movementDirection * _movementSpeed * MOVEMENT_BASE_SPEED * Time.deltaTime;
+    }
+}
