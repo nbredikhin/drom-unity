efe4c855abeab1d3aa950fc63cf3058bf2da45c9
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2019-08-26 19:42:32 +0000

    Death tiles, updated tilesheet

---
+++ Assets/Scripts/DeathTiles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DeathTiles : MonoBehaviour
+{
+    private Tilemap tilemap;
+
+    private void Awake()
+    {
+        tilemap = GetComponent<Tilemap>();
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        var tilemapPosition = tilemap.WorldToCell(collider.gameObject.transform.position);
+        if (!tilemap.HasTile(tilemapPosition))
+            return;
+        // var health = collider.gameObject.GetComponent<Health>();
+        // if (health == null) return;
+
+        Debug.Log("Kill " + collider.gameObject.name);
+        collider.attachedRigidbody.AddForce(-100f * collider.attachedRigidbody.velocity);
+    }
+}
