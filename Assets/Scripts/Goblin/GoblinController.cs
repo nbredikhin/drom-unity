9e6f1a48c3d84225a0824c24b9fb74b22225ac0b
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2019-08-26 10:34:52 +0000

    Pathfinding for enemies

--- Assets/Scripts/Goblin/GoblinController.cs
+++ Assets/Scripts/Goblin/GoblinController.cs
@@ -1,14 +1,28 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class GoblinController : MonoBehaviour
 {
-    [Header("Movement Stats")]
+    [Header("General stuff")]
     [Range(1, 500)] public float MOVEMENT_BASE_SPEED;
+    public LayerMask PathFindObstaclesMask;
+    public bool DrawPath;
+    public Color PathColor = Color.green;
     [Space(10)]
 
     [Header("References")]
     public Rigidbody2D rb;
+
+    [SerializeField] private TilePathFinder _pathFinder;
+    public TilePathFinder PathFinder => _pathFinder;
+
     private GameObject _target;
+    private Vector3 pathTargetPosition;
+    private List<Vector3> currentPath;
+    private float PATH_RECALCULATE_TIME = 2.0f;
+    private float PATH_RECALCULATE_DISTANCE = 0.16f;
+    private int currentPathNode;
+    private float pathUpdateTime = 0;
 
     private void Awake()
     {
@@ -16,24 +30,91 @@ public class GoblinController : MonoBehaviour
         {
             rb = gameObject.GetComponent<Rigidbody2D>();
         }
+        if (_pathFinder == null)
+        {
+            _pathFinder = GameObject.Find("Grid").GetComponent<TilePathFinder>();
+        }
+
         _target = GameObject.Find("Player");
+        RecalculatePath();
+    }
+
+    private void RecalculatePath()
+    {
+        // If path was recalculated too recently
+        if (pathUpdateTime > 0 && Time.time - pathUpdateTime < PATH_RECALCULATE_TIME)
+            return;
+        if (_target == null)
+        {
+            currentPath = null;
+            return;
+        }
+
+        pathUpdateTime = Time.time;
+        pathTargetPosition = _target.transform.position;
+        currentPath = _pathFinder.FindPath(transform.position, pathTargetPosition);
+        if (currentPath != null)
+            currentPathNode = 0;
     }
 
     private void Update()
     {
-        if (_target == null)
+        if (_target == null) return;
+
+        var targetPosition = _target.transform.position;
+        var currentPosition = transform.position;
+        var raycastDirection = targetPosition - currentPosition;
+        var raycastHit = Physics2D.Raycast(
+            currentPosition,
+            raycastDirection.normalized,
+            raycastDirection.magnitude,
+            PathFindObstaclesMask
+        );
+        var isPathClear = raycastHit.collider == null;
+
+        if (isPathClear)
         {
-            return;
+            if (DrawPath)
+                Debug.DrawLine(currentPosition, currentPosition + raycastDirection, PathColor);
+        }
+        else
+        {
+            if (currentPath == null)
+            {
+                // If it's been too long since last path update, recalculate path
+                if (Time.time - pathUpdateTime > PATH_RECALCULATE_TIME) RecalculatePath();
+                return;
+            }
+
+            // If target moved far enough to recalculate path
+            if ((pathTargetPosition - _target.transform.position).magnitude > PATH_RECALCULATE_DISTANCE)
+                RecalculatePath();
+
+            targetPosition = currentPath[currentPathNode];
+
+            if (DrawPath && currentPath.Count > 1)
+            {
+                for (int i = 1; i < currentPath.Count; i++)
+                {
+                    Debug.DrawLine(currentPath[i - 1], currentPath[i], PathColor);
+                }
+            }
         }
 
-        var direction = _target.transform.position - this.transform.position;
-        if (direction.magnitude > 0.15f)
+        // Move goblin
+        var direction = targetPosition - currentPosition;
+        direction.z = 0;
+        if (direction.magnitude > 0.05f)
         {
             rb.velocity = direction.normalized * MOVEMENT_BASE_SPEED * Time.deltaTime;
         }
         else
         {
             rb.velocity = Vector2.zero;
+            if (currentPathNode < currentPath.Count - 1)
+            {
+                currentPathNode = currentPathNode + 1;
+            }
         }
     }
 }
---
+++ Assets/Scripts/Utils/TilePathFinder.cs
@@ -0,0 +1,140 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+public class PathNode
+{
+    public Vector3Int position;
+    public int pathLengthFromStart;
+    public PathNode previousNode;
+    public int heuristicEstimatePathLength;
+
+    public int EstimateFullPathLength
+    {
+        get
+        {
+            return this.pathLengthFromStart + this.heuristicEstimatePathLength;
+        }
+    }
+}
+
+public class TilePathFinder : MonoBehaviour
+{
+    private Grid grid;
+    private List<Tilemap> collisionTilemaps = new List<Tilemap>();
+
+    private void Awake()
+    {
+        grid = this.GetComponent<Grid>();
+
+        var tilemaps = grid.GetComponentsInChildren<Tilemap>();
+        foreach (var tilemap in tilemaps)
+        {
+            if (tilemap.GetComponent<TilemapCollider2D>())
+            {
+                collisionTilemaps.Add(tilemap);
+            }
+        }
+    }
+
+    public List<Vector3> FindPath(Vector2 from, Vector2 to)
+    {
+        var closedSet = new Collection<PathNode>();
+        var openSet = new Collection<PathNode>();
+
+        var startPosition = grid.WorldToCell(from);
+        var finishPosition = grid.WorldToCell(to);
+        PathNode startNode = new PathNode()
+        {
+            position = startPosition,
+            previousNode = null,
+            pathLengthFromStart = 0,
+            heuristicEstimatePathLength = GetHeuristicPathLength(startPosition, finishPosition)
+        };
+
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            var currentNode = openSet.OrderBy(node => node.EstimateFullPathLength).First();
+            if (currentNode.position == finishPosition)
+            {
+                return GetPathFromNode(currentNode);
+            }
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            foreach (var neighbourNode in GetNodeNeighbours(currentNode, finishPosition))
+            {
+                if (closedSet.Count(node => node.position == neighbourNode.position) > 0)
+                    continue;
+                var openNode = openSet.FirstOrDefault(node => node.position == neighbourNode.position);
+                if (openNode == null)
+                {
+                    openSet.Add(neighbourNode);
+                }
+                else if (openNode.pathLengthFromStart > neighbourNode.pathLengthFromStart)
+                {
+                    openNode.previousNode = currentNode;
+                    openNode.pathLengthFromStart = neighbourNode.pathLengthFromStart;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private List<Vector3> GetPathFromNode(PathNode node)
+    {
+        var result = new List<Vector3>();
+        var currentNode = node;
+        while (currentNode != null)
+        {
+            result.Add(grid.CellToWorld(currentNode.position) + Vector3.one * 0.08f);
+            currentNode = currentNode.previousNode;
+        }
+        result.Reverse();
+        return result;
+    }
+
+    private bool GetTilePassable(Vector3Int position)
+    {
+        bool isPassable = false;
+        collisionTilemaps.ForEach(tilemap =>
+            isPassable = isPassable || tilemap.GetTile(position));
+        return !isPassable;
+    }
+
+    private List<PathNode> GetNodeNeighbours(PathNode node, Vector3Int finishPosition)
+    {
+        var neighbourPositions = new Vector3Int[4];
+        neighbourPositions[0] = node.position + new Vector3Int(1, 0, 0);
+        neighbourPositions[1] = node.position + new Vector3Int(-1, 0, 0);
+        neighbourPositions[2] = node.position + new Vector3Int(0, 1, 0);
+        neighbourPositions[3] = node.position + new Vector3Int(0, -1, 0);
+
+        var result = new List<PathNode>();
+        foreach (var pos in neighbourPositions)
+        {
+            if (GetTilePassable(pos))
+            {
+                var neighbourNode = new PathNode()
+                {
+                    position = pos,
+                    previousNode = node,
+                    pathLengthFromStart = node.pathLengthFromStart + 1,
+                    heuristicEstimatePathLength = GetHeuristicPathLength(pos, finishPosition)
+                };
+                result.Add(neighbourNode);
+            }
+        }
+        return result;
+    }
+
+    private int GetHeuristicPathLength(Vector3Int from, Vector3Int to)
+    {
+        return Mathf.Abs(from.x - to.x) + Mathf.Abs(from.y - to.y);
+    }
+}
