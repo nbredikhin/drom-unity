7afc3898d5f8794d766c6c18d5ae286039f76b5d
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2019-08-26 21:21:43 +0000

    Animated doors

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEditor;
+
+public class DoorController : MonoBehaviour
+{
+    public Animator doorAnimator;
+    public GameObject exitGameObject;
+    public GameObject nextCamera;
+
+    void Awake()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            doorAnimator.SetBool("IsOpened", !doorAnimator.GetBool("IsOpened"));
+            Debug.Log(doorAnimator.GetBool("IsOpened"));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (exitGameObject && collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.transform.position = exitGameObject.transform.position;
+            foreach (var camera in GameObject.FindGameObjectsWithTag("Camera"))
+            {
+                if (camera != nextCamera)
+                {
+                    camera.SetActive(false);
+                }
+            }
+            nextCamera.SetActive(true);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (exitGameObject != null)
+            Debug.DrawLine(this.transform.position, exitGameObject.transform.position, Color.blue);
+    }
+}
