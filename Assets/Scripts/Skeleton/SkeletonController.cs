48fc3902d1dc6946256dcd37cfd15130e7b9818e
Evgeniy Morozov <morozov.evgeniy.95@gmail.com>
2019-08-28 20:56:25 +0000

    Update Skeleton animation

---
+++ Assets/Scripts/Skeleton/SkeletonController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonController : MonoBehaviour
+{
+    Animator animator;
+    public bool facingUp;
+    public bool facingRight;
+    public bool moving;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetBool("FacingUp", facingUp);
+        animator.SetBool("FacingRight", facingRight);
+        animator.SetBool("IsMoving", moving);
+    }
+}
