36c4eaeb742b7118085e2110a70b8a03438657c1
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2019-08-29 21:56:51 +0000

    Tutorial text and fixes

--- Assets/Scripts/DeathTiles.cs
+++ Assets/Scripts/DeathTiles.cs
@@ -14,9 +14,12 @@ public class DeathTiles : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collider)
     {
-        var tilemapPosition = tilemap.WorldToCell(collider.gameObject.transform.position);
-        if (!tilemap.HasTile(tilemapPosition))
-            return;
+        if (tilemap != null)
+        {
+            var tilemapPosition = tilemap.WorldToCell(collider.gameObject.transform.position);
+            if (!tilemap.HasTile(tilemapPosition))
+                return;
+        }
 
         collider.gameObject.SendMessage("DecreaseHealth", 10000.0f);
     }
