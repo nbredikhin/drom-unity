cea1714a06d436df6b186395cc35bb3b9d72d809
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2019-08-29 23:22:27 +0000

    Added wall lights

---
+++ Assets/Scripts/Utils/AnimationRandomizer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class AnimationRandomizer : MonoBehaviour
+{
+    void Start()
+    {
+        Animator anim = GetComponent<Animator>();
+        AnimatorStateInfo state = anim.GetCurrentAnimatorStateInfo(0);
+        anim.Play(state.fullPathHash, -1, Random.Range(0f, 1f));
+    }
+}
